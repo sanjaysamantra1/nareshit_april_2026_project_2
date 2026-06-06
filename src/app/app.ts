5a52485670cde880adad53de85df8686eff78641
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-06-06 08:10:40 +0530

    envs

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { Counter } from './components/counter/counter';
 import { TodoCrud } from './components/todo-crud/todo-crud';
 import { EmployeeList } from './components/employee-list/employee-list';
 import { EmployeeAdd } from './components/employee-add/employee-add';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,16 @@ import { EmployeeAdd } from './components/employee-add/employee-add';
     // Counter,
     // TodoCrud
     EmployeeList,
-    EmployeeAdd
+    EmployeeAdd,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('nareshit_april_2026_project_2');
+
+  constructor() {}
+  ngOnInit() {
+    console.log('This is App', environment.apiUrl);
+  }
 }
